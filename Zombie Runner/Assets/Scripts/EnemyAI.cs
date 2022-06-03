b8cd935ef849142c5b0c48f6013d5d1b617b7cd0
Owen Beeks <obeeks13@gmail.com>
2022-06-03 15:50:44 +0100

    Enemy AI - Chase Range

--- Zombie Runner/Assets/Scripts/EnemyAI.cs
+++ Zombie Runner/Assets/Scripts/EnemyAI.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {  
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
     UnityEngine.AI.NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+
     void Start()
     {
         navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
     }
 }
