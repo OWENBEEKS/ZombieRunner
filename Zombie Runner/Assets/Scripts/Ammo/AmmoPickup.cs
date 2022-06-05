3dae73c8c5495f02130538490baee2ad57a5c2c0
Owen Beeks <obeeks13@gmail.com>
2022-06-06 00:16:45 +0100

    Ammo Pickup - Part 2
    Created 3 prefab variants for ammo pickup, altered scrip to add ammo when pickup occurs

--- Zombie Runner/Assets/Scripts/Ammo/AmmoPickup.cs
+++ Zombie Runner/Assets/Scripts/Ammo/AmmoPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("NEW ITEM");
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+            Destroy(gameObject);
         }
     }
 }
