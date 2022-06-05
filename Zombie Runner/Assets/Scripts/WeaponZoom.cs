ff0c39bcf8022175231d9d0d2f53d2339276d9f2
Owen Beeks <obeeks13@gmail.com>
2022-06-05 11:07:32 +0100

    Update WeaponZoom.cs
    Changed sensitivity when zooming

--- Zombie Runner/Assets/Scripts/WeaponZoom.cs
+++ Zombie Runner/Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float normalFOV = 60f;
     [SerializeField] float zoomedFOV = 35f;
+    [SerializeField] float normalSensitivity = 2f;
+    [SerializeField] float zoomedSensitivity = 1f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -18,11 +28,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomedFOV;
+                fpsController.mouseLook.XSensitivity = zoomedSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedSensitivity;
             }
             else
             {
-            zoomedInToggle = false;
-            fpsCamera.fieldOfView = normalFOV;
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = normalFOV;
+                fpsController.mouseLook.XSensitivity = normalSensitivity;
+                fpsController.mouseLook.YSensitivity = normalSensitivity;
             }
         }
     }
