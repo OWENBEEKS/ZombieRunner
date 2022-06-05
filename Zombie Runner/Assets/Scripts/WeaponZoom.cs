2fbb2d374db1ed0c4a2a237ce452f958937acc4f
Owen Beeks <obeeks13@gmail.com>
2022-06-05 12:39:00 +0100

    Weapon tuning

--- Zombie Runner/Assets/Scripts/WeaponZoom.cs
+++ Zombie Runner/Assets/Scripts/WeaponZoom.cs
@@ -5,20 +5,13 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
-    [SerializeField] Camera fpsCamera;
+    [SerializeField] Camera fpsCamera; 
+    [SerializeField] RigidbodyFirstPersonController fpsController;
     [SerializeField] float normalFOV = 60f;
     [SerializeField] float zoomedFOV = 35f;
     [SerializeField] float normalSensitivity = 2f;
     [SerializeField] float zoomedSensitivity = 1f;
-
-    RigidbodyFirstPersonController fpsController;
-
     bool zoomedInToggle = false;
-
-    private void Start()
-    {
-        fpsController = GetComponent<RigidbodyFirstPersonController>();
-    }
 
     private void Update()
     {
